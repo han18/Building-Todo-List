311a241006df2fcac5beecf1129f2ceece841867
han18 <han.fadah@gmail.com>
2024-02-05 13:24:41 -0500

    creating folder compomtents and files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
+import { initialState } from "./Data/data-list";
+import { useReducer } from "react";
 
 function App() {
+  const [todo, dispatch] = useReducer(initialState);
+
   return (
     <div className="App">
       <h1>This is a to do list app</h1>
