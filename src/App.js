fc025373096f0b922a9db0fa7c9aaf7d617fe860
han18 <han.fadah@gmail.com>
2024-02-05 13:50:43 -0500

    fixed an import function to APP.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import { initialState } from "./Data/data-list.js";
 import TodoList from "./Components /TodoList.js";
-import { TodoReducer } from "./Reducer/TodoReducer";
+import { todoReducer } from "./Reducer/todoReducer.js";
 import { useReducer } from "react";
 
 function App() {
-  const [todos, dispatch] = useReducer(TodoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
 
   return (
     <div className="App">
       <h1>Todo App</h1>
 
-      <TodoList todos={todos} dispatch={dispatch} />
+      <TodoList todo={todos} dispatch={dispatch} />
     </div>
   );
 }
