3f5620ad818659ac68db1448e1982126b5011007
han18 <han.fadah@gmail.com>
2024-02-06 05:37:23 -0500

    correction some error

--- src/App.js
+++ src/App.js
@@ -13,77 +13,78 @@ function App() {
       dispatch({ type: "ADD_TODO", payload: newTodo });
       setNewTodo("");
     }
-    const handleToggleTodo = (id) => {
-      dispatch({ type: "TOGGLE_TODO", payload: id });
-    };
-
-    const handleDeleteTodo = (id) => {
-      dispatch({ type: "DELETE_TODO", payload: id });
-    };
-
-    const handleEditTodo = (id, title) => {
-      setEditTodo({ id, title });
-    };
-
-    const handleSaveTodo = () => {
-      dispatch({
-        type: "EDIT_TODO",
-        payload: { id: editTodo.id, title: editTodo.title },
-      });
-      setEditTodo({ id: null, title: "" });
-    };
-
-    return (
-      <div>
-        <h1>Todo List</h1>
-        <input
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Add new todo"
-        />
-        <button onClick={handleAddTodo}>Add Todo</button>
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}>
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => handleToggleTodo(todo.id)}
-              />
-              {editTodo.id === todo.id ? (
-                <>
-                  <input
-                    type="text"
-                    value={editTodo.title}
-                    onChange={(e) =>
-                      setEditTodo({ ...editTodo, title: e.target.value })
-                    }
-                  />
-                  <button onClick={handleSaveTodo}>Save</button>
-                </>
-              ) : (
-                <>
-                  <span>{todo.title}</span>
-                  <button
-                    onClick={() => handleEditTodo(todo.id, todo.title)}
-                    disabled={todo.completed}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => handleDeleteTodo(todo.id)}
-                    disabled={!todo.completed}
-                  >
-                    Delete
-                  </button>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
   };
+
+  const handleToggleTodo = (id) => {
+    dispatch({ type: "TOGGLE_TODO", payload: id });
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({ type: "DELETE_TODO", payload: id });
+  };
+
+  const handleEditTodo = (id, title) => {
+    setEditTodo({ id, title });
+  };
+
+  const handleSaveTodo = () => {
+    dispatch({
+      type: "EDIT_TODO",
+      payload: { id: editTodo.id, title: editTodo.title },
+    });
+    setEditTodo({ id: null, title: "" });
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="add to the list"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggleTodo(todo.id)}
+            />
+            {editTodo.id === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  value={editTodo.title}
+                  onChange={(e) =>
+                    setEditTodo({ ...editTodo, title: e.target.value })
+                  }
+                />
+                <button onClick={handleSaveTodo}>Save</button>
+              </>
+            ) : (
+              <>
+                <span>{todo.title}</span>
+                <button
+                  onClick={() => handleEditTodo(todo.id, todo.title)}
+                  disabled={todo.completed}
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => handleDeleteTodo(todo.id)}
+                  disabled={!todo.completed}
+                >
+                  Delete
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default App;
--- src/reducer/todoReducer.js
+++ src/reducer/todoReducer.js
@@ -1,19 +1,19 @@
 export const todoReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TODO":
+    case "ADD":
       return [
         { id: Date.now(), title: action.payload, completed: false },
         ...state,
       ];
-    case "TOGGLE_TODO":
+    case "TOGGLE":
       return state.map((todo) =>
         todo.id === action.payload
           ? { ...todo, completed: !todo.completed }
           : todo
       );
-    case "DELETE_TODO":
+    case "DELETE":
       return state.filter((todo) => todo.id !== action.payload);
-    case "EDIT_TODO":
+    case "EDIT":
       return state.map((todo) =>
         todo.id === action.payload.id
           ? { ...todo, title: action.payload.title }
